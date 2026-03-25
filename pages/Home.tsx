ba375e1d5110acff5b68ee9d7ed0be259241acd4
Rémi Guillette <remiguillette@gmail.com>
2026-03-25 03:54:13 -0400

    Create Home.tsx

---
+++ pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Bienvenue</h1>
+        <p>Site vitrine simple et propre.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
