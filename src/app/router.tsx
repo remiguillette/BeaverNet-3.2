c80dc317a536cf1444fda8314185ff6df58eedec
Rémi Guillette <remiguillette@gmail.com>
2026-03-25 09:32:08 -0400

    Add privacy policy page and wire route

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,11 +4,13 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import ServicePage from "../pages/ServicePage";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/about", element: <About /> },
   { path: "/contact", element: <Contact /> },
+  { path: "/privacy-policy", element: <PrivacyPolicy /> },
   { path: "/public-safety", element: <ServicePage serviceKey="publicSafety" /> },
   {
     path: "/francophone-services",
