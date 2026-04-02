17d9008b48e4a58e3ba285ab234614b24690061a
Rémi Guillette <remiguillette@gmail.com>
2026-04-02 02:14:37 -0400

    Revert "Temporarily route all traffic to server maintenance page"

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-import Maintenance from "../pages/Maintenance";
-
-export const TEMPORARY_MAINTENANCE_PATH = "/maintenance";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+import ServicePage from "../pages/ServicePage";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
 
 export const router = createBrowserRouter([
-  { path: TEMPORARY_MAINTENANCE_PATH, element: <Maintenance /> },
-  { path: "*", element: <Maintenance /> },
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
+  { path: "/privacy-policy", element: <PrivacyPolicy /> },
+  { path: "/public-safety", element: <ServicePage serviceKey="publicSafety" /> },
+  {
+    path: "/francophone-services",
+    element: <ServicePage serviceKey="francophoneServices" />,
+  },
+  { path: "/health-safety", element: <ServicePage serviceKey="healthSafety" /> },
+  { path: "/animal-first-aid", element: <ServicePage serviceKey="animalFirstAid" /> },
+  { path: "*", element: <NotFound /> },
 ]);
